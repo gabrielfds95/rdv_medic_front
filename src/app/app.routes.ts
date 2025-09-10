7b59ac001e1da66425273fbc87f9d2e3daa41612
gabrielfds95 <fernandesgabriel.pro@gmail.com>
2025-09-10 16:38:57 +0200

    fix routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,10 @@
 //Angular regarde l’URL et affiche le composant correspondant dans <router-outlet>
 
 import { Routes } from '@angular/router';
+import { DoctorListComponent } from './component/doctor/doctor-list.component';
+import { SlotListComponent } from './component/slot/slot-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: DoctorListComponent },             // page d'accueil : liste des médecins
+  { path: 'doctor-slots/:id', component: SlotListComponent } // créneaux d’un médecin
+];
